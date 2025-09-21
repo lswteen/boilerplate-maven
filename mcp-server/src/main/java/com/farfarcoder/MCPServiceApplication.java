bf4037f73c43981fcac289cb3b58fff05eba5d97
leesoonwoo <lswteen@gmail.com>
2025-09-21 09:38:47 +0900

    add : mcp server 추가

---
+++ mcp-server/src/main/java/com/farfarcoder/MCPServiceApplication.java
@@ -0,0 +1,14 @@
+package com.farfarcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class MCPServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MCPServiceApplication.class,args);
+    }
+}
---
+++ mcp-server/src/test/java/com/farfarcoder/MCPServiceApplicationTest.java
@@ -0,0 +1,19 @@
+package com.farfarcoder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MCPServiceApplicationTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+//    @Test
+//    public void shouldAnswerWithTrue() {
+//        assertTrue(true);
+//    }
+}
